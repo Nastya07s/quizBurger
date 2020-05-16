33a03f6e6f9c52e7ea223a49fad36367992b24cc
Nastya07s <nastya.kotovich07@gmail.com>
2020-05-16 23:16:48 +0300

    feat: add burger button

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
   const btnOpenModal = document.querySelector('#btnOpenModal');
   const modalBlock = document.querySelector('#modalBlock');
+  // const modalWrap = document.querySelector('.modal');
   const closeModal = document.querySelector('#closeModal');
   const questionTitle = document.querySelector('#question');
   const formAnswers = document.querySelector('#formAnswers');
+  const burgerBtn = document.getElementById('burger');
+
+  let { clientWidth } = document.documentElement;
+  burgerBtn.style.display = clientWidth < 768 ? 'flex' : 'none';
+
+  window.addEventListener('resize', () => {
+    clientWidth = document.documentElement.clientWidth;
+
+    burgerBtn.style.display = clientWidth < 768 ? 'flex' : 'none';
+  });
 
   const playTest = () => {
     const renderQuestions = () => {
@@ -25,13 +36,30 @@ document.addEventListener('DOMContentLoaded', () => {
     renderQuestions();
   };
 
+  burgerBtn.addEventListener('click', () => {
+    burgerBtn.classList.add('active');
+    modalBlock.classList.add('d-block');
+
+    playTest();
+  });
+
   btnOpenModal.addEventListener('click', () => {
     modalBlock.classList.add('d-block');
 
     playTest();
   });
 
+  document.body.addEventListener('click', ({ target }) => {
+    if (!target.closest('.modal-dialog')
+    && !target.closest('.openModalButton')
+    && !target.closest('.burger')) {
+      modalBlock.classList.remove('d-block');
+      burgerBtn.classList.remove('active');
+    }
+  });
+
   closeModal.addEventListener('click', () => {
     modalBlock.classList.remove('d-block');
+    burgerBtn.classList.remove('active');
   });
 });
